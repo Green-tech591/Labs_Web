4ad627bd94e30a01424e6350640b09309801cee2
Green-tech591 <55706432+Green-tech591@users.noreply.github.com>
2022-12-12 09:15:00 +0300

    Add files via upload

---
+++ Лаба4/main.js
@@ -0,0 +1,24 @@
+let categories_button = document.querySelectorAll(".categories_button");
+
+const FilterMainItems = categorie => {
+    let mainItems = document.querySelectorAll(".post[data-categories]");
+
+    mainItems.forEach(item => {
+        let categoriesItem = item.dataset.categories.split(', ');
+
+        let result = categoriesItem.find(it => {
+            return categorie === it;
+        });
+
+        if (item.classList.contains("block"))
+            item.classList.remove("block")
+        else
+            item.classList.remove("hide")
+
+        item.classList.add(result ? "block" : "hide");
+    })
+};
+
+categories_button.forEach(it => {
+    it.addEventListener("click", () => { FilterMainItems(it.dataset.categories) });
+});
